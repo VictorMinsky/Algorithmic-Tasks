e4efd6932e3a65793551c23189e6ed4565532240
VictorMinsky <panasyuk.vityu@gmail.com>
2019-09-28 21:01:31 +0300

    Create Integers Recreation One.py

---
+++ Codewars/5 kyu/Integers Recreation One.py
@@ -0,0 +1,36 @@
+"""
+Divisors of 42 are : 1, 2, 3, 6, 7, 14, 21, 42. These divisors squared are: 1, 4, 9, 36, 49, 196, 441, 1764. The sum of the squared divisors is 2500 which is 50 * 50, a square!
+
+Given two integers m, n (1 <= m <= n) we want to find all integers between m and n whose sum of squared divisors is itself a square. 42 is such a number.
+
+The result will be an array of arrays or of tuples (in C an array of Pair) or a string, each subarray having two elements, first the number whose squared divisors is a square and then the sum of the squared divisors.
+
+#Examples:
+
+list_squared(1, 250) --> [[1, 1], [42, 2500], [246, 84100]]
+list_squared(42, 250) --> [[42, 2500], [246, 84100]]
+The form of the examples may change according to the language, see Example Tests: for more details.
+
+Note
+
+In Fortran - as in any other language - the returned string is not permitted to contain any redundant trailing whitespace: you can use dynamically allocated character strings.
+"""
+
+
+
+from functools import reduce
+from math import sqrt
+
+def factors(n):    
+    return set(reduce(list.__add__, 
+                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+
+
+def list_squared(m, n):
+    nums = []
+    for i in range(m,n+1):
+        fc = factors(i)
+        sm = sum(map(lambda x:x**2, fc))
+        if sqrt(sm).is_integer():
+            nums.append([i, sm])
+    return nums
