5fa3e8e221693f3a1f8dc507a06376cba3eb382c
VictorMinsky <panasyuk.vityu@gmail.com>
2019-10-03 22:13:04 +0300

    Create Consecutive strings.py

---
+++ Codewars/6 kyu/Consecutive strings.py
@@ -0,0 +1,22 @@
+"""
+You are given an array strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+
+Example:
+longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+
+n being the length of the string array, if n = 0 or k > n or k <= 0 return "".
+
+Note
+consecutive strings : follow one after another without an interruption
+"""
+
+
+
+def longest_consec(strarr, k):
+    ans = ''
+    if not len(strarr) or k > len(strarr) or k <= 0:
+        return ''
+    for i in range(len(strarr) - k + 1):
+        if len(''.join(strarr[i:i + k])) > len(ans):
+            ans = ''.join(strarr[i:i + k])
+    return ans
