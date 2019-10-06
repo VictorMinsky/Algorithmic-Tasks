4249be0a9bda87d07b30c0bf18ebcee3504e30b7
VictorMinsky <panasyuk.vityu@gmail.com>
2019-10-06 15:25:42 +0300

    Create Even or Odd.py

---
+++ Codewars/8 kyu/Even or Odd.py
@@ -0,0 +1,8 @@
+"""
+Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+"""
+
+
+
+def even_or_odd(number):
+    return "Even" if not number % 2 else "Odd"
