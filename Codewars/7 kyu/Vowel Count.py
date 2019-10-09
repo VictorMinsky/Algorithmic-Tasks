e871018e3e090dd11340621abf65a56ff918de73
VictorMinsky <panasyuk.vityu@gmail.com>
2019-10-09 23:46:57 +0300

    Update Vowel Count.py

--- Codewars/7 kyu/Vowel Count.py
+++ Codewars/7 kyu/Vowel Count.py
@@ -5,3 +5,11 @@ We will consider a, e, i, o, and u as vowels for this Kata.
 
 The input string will only consist of lower case letters and/or spaces.
 """
+
+
+
+def getCount(inputStr):
+    num_vowels = 0
+    num_vowels = inputStr.count('a') + inputStr.count('e') + inputStr.count('i') + inputStr.count('o') + inputStr.count(
+        'u')
+    return num_vowels
