34e5321a77c6a99c90c6309d7915b1e2bcf406c8
VictorMinsky <panasyuk.vityu@gmail.com>
2019-10-12 22:58:07 +0300

    Create Perimeter of a Rectangle.py

---
+++ Codewars/8 kyu/Perimeter of a Rectangle.py
@@ -0,0 +1,10 @@
+"""
+Complete the method to calculate the perimeter of a rectangle using given width and length.
+
+You don't need to validate input parameters, all tests has valid arguments (positive integers).
+"""
+
+
+
+def get_perimeter(width, length):
+    return 2 * (width + length)
