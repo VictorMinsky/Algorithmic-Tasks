0a840650338b53f9ded6e476dd3bb693b1f40990
VictorMinsky <panasyuk.vityu@gmail.com>
2019-10-15 23:17:39 +0300

    Update Plus - minus - plus - plus - ... - Count.py

--- Codewars/7 kyu/Plus - minus - plus - plus - ... - Count.py
+++ Codewars/7 kyu/Plus - minus - plus - plus - ... - Count.py
@@ -17,3 +17,18 @@ const arr = [1, -3, -4, 0, 5]
 
 return 2;
 """
+
+
+
+def catch_sign_change(lst):
+    if not lst:
+        return 0
+    sign = 0 if lst[0] < 0 else 1
+    ans = 0
+    for i in lst:
+        if (0 if i < 0 else 1) == sign:
+            continue
+        else:
+            ans += 1
+            sign = 0 if sign == 1 else 1
+    return ans
