adc5ff9c28c2a38910cd7636e9e7898b2ce7292d
VictorMinsky <panasyuk.vityu@gmail.com>
2019-10-19 07:55:37 +0300

    Update Who likes it.py

--- Codewars/6 kyu/Who likes it.py
+++ Codewars/6 kyu/Who likes it.py
@@ -10,3 +10,17 @@ likes ["Max", "John", "Mark"] // must be "Max, John and Mark like this"
 likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
 For 4 or more names, the number in and 2 others simply increases.
 """
+
+
+
+def likes(names):
+    if not names:
+        return 'no one likes this'
+    if len(names) == 1:
+        return "{} likes this".format(*names)
+    if len(names) == 2:
+        return "{} and {} like this".format(*names)
+    if len(names) == 3:
+        return "{}, {} and {} like this".format(*names)
+    else:
+        return "{}, {} and {} others like this".format(*names[:2], len(names[2:]))
