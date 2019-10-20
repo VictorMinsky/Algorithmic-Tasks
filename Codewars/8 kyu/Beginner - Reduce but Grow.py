3af40192f3329968a89fb72d457ceb8c033bfaca
VictorMinsky <panasyuk.vityu@gmail.com>
2019-10-20 19:52:30 +0300

    Create Beginner - Reduce but Grow.py

---
+++ Codewars/8 kyu/Beginner - Reduce but Grow.py
@@ -0,0 +1,10 @@
+"""
+Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+"""
+
+
+
+def grow(arr):
+    return eval('*'.join(map(str, arr)))
