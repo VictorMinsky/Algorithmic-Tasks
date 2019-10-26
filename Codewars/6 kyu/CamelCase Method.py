455849c484b88a008f3ee8b44e69034dab171a12
VictorMinsky <panasyuk.vityu@gmail.com>
2019-10-26 17:49:39 +0300

    Create CamelCase Method.py

---
+++ Codewars/6 kyu/CamelCase Method.py
@@ -0,0 +1,14 @@
+"""
+Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. All words must have their first letter capitalized without spaces.
+
+For instance:
+
+camelcase("hello case") => HelloCase
+camelcase("camel case word") => CamelCaseWord
+Don't forget to rate this kata! Thanks :)
+"""
+
+
+
+def camel_case(string):
+    return ''.join(string.title().strip().split())
