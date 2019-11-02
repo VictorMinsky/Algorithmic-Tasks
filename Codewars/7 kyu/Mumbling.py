5fdeab826c8ad90ccb464d0058d6311b3dccb82e
VictorMinsky <panasyuk.vityu@gmail.com>
2019-11-02 16:33:20 +0300

    Create Mumbling.py

---
+++ Codewars/7 kyu/Mumbling.py
@@ -0,0 +1,18 @@
+"""
+This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+"""
+
+
+
+def accum(s):
+    ans = []
+    for index, key in enumerate(s):
+        ans.append((key * (index + 1)).title())
+    return '-'.join(ans)
