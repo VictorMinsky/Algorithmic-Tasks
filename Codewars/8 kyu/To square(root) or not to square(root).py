3693d34b4e5d341e7d0b53010c13709cc3614997
VictorMinsky <panasyuk.vityu@gmail.com>
2019-11-23 23:22:46 +0300

    Create To square(root) or not to square(root).py

---
+++ Codewars/8 kyu/To square(root) or not to square(root).py
@@ -0,0 +1,31 @@
+"""
+#To square(root) or not to square(root)
+
+Write a method, that will get an integer array as parameter and will process every number from this array.
+Return a new array with processing every number of the input-array like this:
+
+If the number has an integer square root, take this, otherwise square the number.
+
+[4,3,9,7,2,1] -> [2,9,3,49,4,1]
+The input array will always contain only positive numbers and will never be empty or null.
+
+The input array should not be modified!
+
+Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+I have also created other katas. Take a look if you enjoyed this kata!
+"""
+
+
+
+from math import sqrt
+
+
+def square_or_square_root(arr):
+    ans = []
+    for element in arr:
+        if sqrt(element).is_integer():
+            ans.append(sqrt(element))
+        else:
+            ans.append(element ** 2)
+    return ans
