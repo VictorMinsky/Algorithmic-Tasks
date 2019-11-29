cef587b70e1783cf4023c4a7245cb2ae6bec46a1
VictorMinsky <panasyuk.vityu@gmail.com>
2019-11-29 21:49:22 +0300

    Create Area of a Square.py

---
+++ Codewars/8 kyu/Area of a Square.py
@@ -0,0 +1,15 @@
+"""
+Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
+
+Graph
+
+Note: use the π value provided in your language (Math::PI, M_PI, math.pi, etc)
+"""
+
+
+
+from math import pi
+
+
+def square_area(A):
+    return round((2 * A / pi) ** 2, 2)
