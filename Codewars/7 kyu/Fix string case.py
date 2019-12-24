4433260fc75bfba93ce213b6a6b362600b1e631d
VictorMinsky <panasyuk.vityu@gmail.com>
2019-12-24 21:46:26 +0300

    Create Fix string case.py

---
+++ Codewars/7 kyu/Fix string case.py
@@ -0,0 +1,32 @@
+"""
+In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
+
+make as few changes as possible.
+if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
+For example:
+
+solve("coDe") = "code". Lowercase characters > uppercase. Change only the "D" to lowercase.
+solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
+solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
+More examples in test cases. Good luck!
+
+Please also try:
+
+Simple time difference
+
+Simple remove duplicates
+"""
+
+
+
+def solve(s):
+    lower, upper = 0, 0
+    for i in s:
+        if i.isupper():
+            upper += 1
+        else:
+            lower += 1
+    if upper > lower:
+        return s.upper()
+    else:
+        return s.lower()
