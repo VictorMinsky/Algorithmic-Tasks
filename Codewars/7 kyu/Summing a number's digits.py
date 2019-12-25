0774e431c1154981c895de2f5cebf9b4c7b7fba1
VictorMinsky <panasyuk.vityu@gmail.com>
2019-12-25 21:26:05 +0300

    Create Summing a number's digits.py

---
+++ Codewars/7 kyu/Summing a number's digits.py
@@ -0,0 +1,13 @@
+"""
+Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits. For example:
+
+  sum_digits(10)  # Returns 1
+  sum_digits(99)  # Returns 18
+  sum_digits(-32) # Returns 5
+Let's assume that all numbers in the input will be integer values.
+"""
+
+
+
+def sum_digits(number):
+    return sum([int(i) for i in list(str(abs(number)))])
