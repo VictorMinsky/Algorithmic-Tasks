5d9d9eda55e563f18eff8a01e932a022f5d60290
VictorMinsky <panasyuk.vityu@gmail.com>
2019-12-26 21:34:41 +0300

    Create Find the capitals.py

---
+++ Codewars/7 kyu/Find the capitals.py
@@ -0,0 +1,12 @@
+"""
+Instructions
+Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+Example
+Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+"""
+
+
+
+def capitals(word):
+    return [i for i in range(len(word)) if word[i].isupper()]
