8616addb333cdd15c39307634c54657a4ca2b2c0
VictorMinsky <panasyuk.vityu@gmail.com>
2019-12-31 22:17:21 +0300

    Create Reverse a Number.py

---
+++ Codewars/7 kyu/Reverse a Number.py
@@ -0,0 +1,15 @@
+"""
+Given a number, write a function to output its reverse digits. (e.g. given 123 the answer is 321)
+
+Numbers should preserve their sign; i.e. a negative number should still be negative when reversed.
+
+Examples
+ 123 ->  321
+-456 -> -654
+1000 ->    1
+"""
+
+
+
+def reverse_number(n):
+    return -1 * int(str(n)[:0:-1]) if n < 0 else int(str(n)[::-1])
