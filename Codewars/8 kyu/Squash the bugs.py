ed2e694f7892f7edf8e168436a621ef60afbdcb0
VictorMinsky <panasyuk.vityu@gmail.com>
2020-01-05 22:12:41 +0300

    Create Squash the bugs.py

---
+++ Codewars/8 kyu/Squash the bugs.py
@@ -0,0 +1,17 @@
+"""
+Simple challenge - eliminate all bugs from the supplied code so that the code runs and outputs the expected value. Output should be the length of the longest word, as a number.
+
+There will only be one 'longest' word.
+"""
+
+
+
+def find_longest(string):
+    spl = string.split()
+    longest = 0
+    i = 0
+    while i < len(spl):
+        if len(spl[i]) > longest:
+            longest = len(spl[i])
+        i += 1
+    return longest
