3be1a3744f2988a9c62283551f7285830a5ca008
VictorMinsky <panasyuk.vityu@gmail.com>
2020-01-14 21:06:25 +0300

    Create Finding length of the sequence.py

---
+++ Codewars/7 kyu/Finding length of the sequence.py
@@ -0,0 +1,20 @@
+"""
+As part of this Kata, you need to find the length of the sequence in an array, between the first and the second occurrence of a specified number.
+
+For example, for a given array arr
+
+[0, -3, 7, 4, 0, 3, 7, 9]
+Finding length between two 7s like
+
+lengthOfSequence([0, -3, 7, 4, 0, 3, 7, 9], 7)
+would return 5.
+
+For sake of simplicity, there will only be numbers (positive or negative) in the supplied array.
+
+If there are less or more than two occurrences of the number to search for, return 0, or in Haskell, Nothing.
+"""
+
+
+
+def length_of_sequence(arr, n):
+    return 0 if arr.count(n) != 2 else arr.index(n, arr.index(n) + 1) - arr.index(n) + 1
